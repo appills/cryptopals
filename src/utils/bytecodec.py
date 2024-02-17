6b0babbba2366de6d4cfd080169aed1593b2da19
appills <adpilipauskas@gmail.com>
2024-02-17 09:48:36 -0600

    add string to bytes methods

--- src/utils/bytecodec.py
+++ src/utils/bytecodec.py
@@ -15,4 +15,10 @@ def bytes_to_base64(buf) -> str:
     return b64encode(buf).decode()
 # b64_str: str, return bytes
 def base64_to_bytes(b64_str) -> bytes:
-    return b64decode(b64_str) 
+    return b64decode(b64_str)
+# bytes b'foobar' to utf-8 string ('foobar')
+def bytes_to_utf8(byts: bytes) -> str:
+    return bytes.decode(byts, 'utf-8') 
+# utf-8 string (e.g. 'foobar'), return bytes (b'foobar')
+def utf8_to_bytes(s: str) -> bytes:
+    return s.encode('utf-8')
